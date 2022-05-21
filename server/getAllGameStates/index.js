809a58685071b4cc6b26c766fb05d9b82527b1bb
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-05-21 11:27:06 +0200

    Finish first real endpoint

--- server/getAllGameStates/index.js
+++ server/getAllGameStates/index.js
@@ -1,10 +1,43 @@
+import pg from "pg";
+const { Client } = pg;
+
+//TODO remove credentials
+const client = new Client({
+  user: "postgres",
+  password: "x$ksLfuV7WD8ECKN7^Wf",
+  host: "lets-go.c6pcl5yvnycx.eu-north-1.rds.amazonaws.com",
+  database: "lets-go",
+  port: 5432,
+});
+
 const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
+  let response;
+  try {
+    await client.connect();
+    console.log("Connected to DB");
+    try {
+      const result = await client.query("SELECT * FROM game_state");
+      response = {
+        statusCode: 200,
+        body: JSON.stringify(result.rows),
+      };
+    } catch (error) {
+      console.log(error);
+      response = {
+        statusCode: 500,
+        body: JSON.stringify("Error executing query"),
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify("Could not connect to database"),
+    };
+  } finally {
+    client.end();
+    return response;
+  }
 };
 
 export { handler };
