e7bb98d1abda917f84468ed364e96acb2ce30217
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-08-06 08:32:11 +0200

    Add multiplayer button

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import { Typography } from "@mui/material";
+import { Menu, Typography, MenuItem, Stack, Button } from "@mui/material";
+import { useState } from "react";
 
 const TopBar = () => {
+  const [menuAnchor, setMenuAnchor] = useState(null);
+
+  const handleMenu = (event) => {
+    setMenuAnchor(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setMenuAnchor(null);
+  };
+
+  const handleCreateGame = () => {};
+
   return (
     <React.Fragment>
       <AppBar sx={{ width: "100%" }}>
         <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              fontWeight: 700,
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Let's Go
-          </Typography>
+          <Stack direction="row" spacing={3}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                fontWeight: 700,
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Let's Go
+            </Typography>
+            <Button variant="text" onClick={handleMenu} color="inherit">
+              Multiplayer
+            </Button>
+            <Menu
+              id="menu-appbar"
+              anchorEl={menuAnchor}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(menuAnchor)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+            </Menu>
+          </Stack>
         </Toolbar>
       </AppBar>
       <Toolbar />
