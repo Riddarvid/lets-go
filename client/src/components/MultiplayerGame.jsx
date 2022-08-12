5f0dfa946161dc56e43ce873e1bfa5dfc4b4de91
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-08-13 00:28:58 +0200

    Refactor snackbar

--- client/src/components/MultiplayerGame.jsx
+++ client/src/components/MultiplayerGame.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import useMultiPlayerGameEngine from "../hooks/MultiPlayerGameEngine";
 import Game from "./Game";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { Box, CircularProgress } from "@mui/material";
+import { useSnackbar } from "notistack";
+import { useEffect } from "react";
 
 const MultiplayerGame = ({ dimension, boardSize }) => {
   const { uuid } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
   const gameEngine = useMultiPlayerGameEngine({ dimension, uuid });
-  if (gameEngine.error) {
-    return <Navigate to={"/"} />;
-  }
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (gameEngine.error) {
+      enqueueSnackbar("No game with that id found.", { variant: "error" });
+      navigate("/");
+    }
+  }, [gameEngine.error]);
+
   if (gameEngine.loading) {
     return (
       <Box
