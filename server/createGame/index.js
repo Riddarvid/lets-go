dd507e98cd60901252ded7efcb13c432dcb3f602
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-08-23 00:44:00 +0200

    Add endpoint for creating new games

--- server/createGame/index.js
+++ server/createGame/index.js
@@ -1,3 +1,83 @@
 import crypto from "crypto";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} from "@aws-sdk/client-apigatewaymanagementapi";
 
-export const handler = async (event, context) => {};
+//DB connection outside handler for sharing
+const dbClient = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(dbClient);
+
+//Everything breaks if there is a solution. Will probably have to use a real RDS
+//to be able to avoid this.
+const createGame = async (size) => {
+  const gameId = crypto.randomUUID();
+  const blackId = crypto.randomUUID();
+  const whiteId = crypto.randomUUID();
+
+  const command = new PutCommand({
+    TableName: process.env.TABLE,
+    Item: {
+      GameId: gameId,
+      BlackId: blackId,
+      WhiteId: whiteId,
+      BlackTurn: true,
+      SquareData: Array(size * size)
+        .fill(0)
+        .join(""),
+    },
+  });
+
+  await docClient.send(command);
+  return {
+    blackId: blackId,
+    whiteId: whiteId,
+  };
+};
+
+const notifyCreator = async (event, ids) => {
+  //Establish callback url
+  const domain = event.requestContext.domainName;
+  const stage = event.requestContext.stage;
+  const callbackUrl = `https://${domain}/${stage}`; //Same api as message was received from.
+  const client = new ApiGatewayManagementApiClient({ endpoint: callbackUrl });
+
+  const command = new PostToConnectionCommand({
+    ConnectionId: event.requestContext.connectionId, //Send back to sender
+    Data: JSON.stringify({
+      type: "game-created",
+      data: ids,
+    }),
+  });
+
+  await client.send(command);
+};
+
+export const handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+
+  let ids;
+  try {
+    ids = await createGame(body.size);
+  } catch (error) {
+    console.error("Error creating game: ", error);
+    return {
+      statusCode: 500,
+    };
+  }
+
+  try {
+    notifyCreator(event, ids);
+  } catch (error) {
+    console.error("Error notifying creator.");
+    return {
+      statusCode: 500,
+    };
+  }
+  return {
+    statusCode: 200,
+  };
+};
