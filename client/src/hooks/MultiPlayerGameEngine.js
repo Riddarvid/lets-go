cdc7ce75504fca478a92ad0d42297f70c187bff2
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-08-23 12:04:31 +0200

    Migrate multiplayer client

--- client/src/hooks/MultiPlayerGameEngine.js
+++ client/src/hooks/MultiPlayerGameEngine.js
@@ -5,48 +5,51 @@ import {
   stringToSquares,
 } from "../helpers/gameLogic";
 
-const backendUrl =
-  "https://gqzmvnpow7.execute-api.eu-north-1.amazonaws.com/test";
+const apiUrl =
+  "wss://b93z3z9h0c.execute-api.eu-north-1.amazonaws.com/production";
 
 const useMultiPlayerGameEngine = ({ dimension, uuid }) => {
   const [gameState, setGameState] = useState(null);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
   const gameLogic = useRef(null);
-
-  const fetchGameState = useCallback(async () => {
-    if (!uuid) {
-      return;
-    }
-    try {
-      const response = await fetch(backendUrl + "/game?uuid=" + uuid);
-      const result = await response.json();
-      if (!result.gameState) {
-        setError(true);
-        return;
-      }
-      const newGameState = { ...result.gameState };
-      newGameState.squares = stringToSquares(newGameState.squares);
-      newGameState.dimension = Math.sqrt(newGameState.squares.length);
-      setGameState(newGameState);
-      setLoading(false);
-      gameLogic.current = new GameLogic(newGameState.dimension);
-    } catch (error) {
-      console.log("Error fetching multiplayer game");
-    }
-  }, [uuid]);
+  const [ws, setWs] = useState(null);
 
   useEffect(() => {
-    fetchGameState();
-  }, [uuid, fetchGameState]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      fetchGameState();
-    }, 2 * 1000); //Refresh every 2 seconds
-
-    return () => clearInterval(interval);
-  }, [fetchGameState]);
+    if (ws === null) {
+      ws = new WebSocket(apiUrl);
+      ws.onmessage = (event) => {
+        const msg = JSON.parse(event.data);
+        switch (msg.type) {
+          case "game-state":
+            const newGameState = { ...msg.data };
+            newGameState.squares = stringToSquares(newGameState.squares);
+            newGameState.dimension = Math.sqrt(newGameState.squares.length);
+            setGameState(newGameState);
+            setLoading(false);
+            gameLogic.current = new GameLogic(newGameState.dimension);
+            break;
+          default:
+            break;
+        }
+      };
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({
+            action: "registerConnection",
+            uuid: uuid,
+          })
+        );
+        ws.send(
+          JSON.stringify({
+            action: "getGame",
+            uuid: uuid,
+          })
+        );
+      };
+      setWs(ws);
+    }
+  }, [ws, uuid]);
 
   const onSquareClicked = async (row, column) => {
     const newSquareData = gameLogic.current.executeMove(gameState, row, column);
@@ -55,14 +58,13 @@ const useMultiPlayerGameEngine = ({ dimension, uuid }) => {
       newGameState.squares = newSquareData;
       newGameState.turn = getOppositeColor(gameState.turn);
       setGameState(newGameState);
-      await fetch(backendUrl + "/move", {
-        method: "POST",
-        body: JSON.stringify({
+      ws.send(
+        JSON.stringify({
           uuid,
           row,
           column,
-        }),
-      });
+        })
+      );
     }
   };
 
